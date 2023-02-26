1ec0436b108c8714c6a2754308d790b34e72a750
Marina <95050756+Marindala@users.noreply.github.com>
2023-02-26 17:14:01 -0300

    solucion problema app

--- FRONT_VIDEOGAMES/src/App.jsx
+++ FRONT_VIDEOGAMES/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from "./components/Navbar/Navbar"
 import SearchBar from './components/SearchBar/SearchBar'
+import CreateUser from './views/createUser/createUser'
 
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
      
     <div>
       <Navbar/>
+      <CreateUser/>
     </div>
     </div>
   )
