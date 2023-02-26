2766f27e5d425ecf221eb330fb7841c87435aa5c
facundo-maciel <maci.facundo@gmail.com>
2023-02-26 16:30:50 -0300

    hice el navbar con el ogo y el nombre temporal, tambien le puse los iconos que va a usar

--- FRONT_VIDEOGAMES/src/App.jsx
+++ FRONT_VIDEOGAMES/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
+import NavBar from './components/Navbar/Navbar'
 
 
 function App() {
 
   return (
     <div className="App">
-     
-      <h1 class="text-xl font-bold">
+
+
+     <NavBar/>
+      {/* <h1 class="text-xl font-bold">
         Hello world...
         Soy Juan
         yo Pablo
-      </h1>
+      </h1> */}
     </div>
   )
 }
