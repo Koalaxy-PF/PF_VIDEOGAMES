609d6c0dadea9adda2f6f61ae12d9fd501f01682
AgusCorcico <corcicoagustin@gmail.com>
2023-02-28 17:49:59 -0300

    delete genre

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,10 +3,12 @@ const router = Router();
 
 const getGenres = require('./genreRoutes/get')
 const postGenres = require('./genreRoutes/post')
+const deleteGenres = require('./genreRoutes/delete')
 
 /* *************************  routes GENRES   **************************************** */
 router.use("/genres", getGenres);
 router.use("/genres", postGenres);
+router.use("/genres", deleteGenres);
 
 
 
