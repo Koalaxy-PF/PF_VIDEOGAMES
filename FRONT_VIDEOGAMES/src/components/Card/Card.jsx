2bff924d5f287ca5b26531d61023ab92c3cf2094
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-01 17:39:00 -0500

    cambios cards

--- FRONT_VIDEOGAMES/src/components/Card/Card.jsx
+++ FRONT_VIDEOGAMES/src/components/Card/Card.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import info from '../../assets/card/info.png'
+import forest from '../../assets/card/forest.jpg'
+import heart from '../../assets/card/heartBlue.png'
+import carrito from '../../assets/card/carrito.png'
+import starWhite from '../../assets/card/starWhite.png'
+import starBlue from '../../assets/card/starBlue.png'
 
 
 // export default function Card({ name, image, price, description }) {
@@ -23,29 +28,49 @@ import info from '../../assets/card/info.png'
 
 export default function Card (){
     return (
-        <div class='justify-center m-auto flex flex-wrap my-[50px] ml-[210px]'>
-            <div class='mx-[10px] p-[50px] border border-solid border-black rounded '>
-             <button><img class='w-[20px] text-right relative left-[195px] bottom-[20px]' src={info} alt="" /></button>
-            <h1 class ='text-center mb-[40px] border border-solid border-black rounded p-[80px]'>Imagen</h1>
-            <h3>Name</h3>
-            <h5>Price</h5>
-            <p>Description</p>
+        <div class='justify-center m-auto flex flex-wrap my-[50px] ml-[210px] bg-[#f6f6f6] pb-[50px]'>
+            <div class='bg-white rounded-lg h-[300px] mx-[10px]'>
+             <button><img class='w-[20px] text-right relative left-[265px] bg-black z-30 top-[5px]'  src={heart} alt="" /></button>
+            <img class='w-[300px] relative bottom-[30px] rounded-t-lg' src={forest} alt="" />
+            <ul class='flex relative bottom-[52px] left-[210px]'>
+                <li><img class='w-[16px]'  src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+            </ul>
+            <h3 class= 'pl-[20px] text-[25px] mb-[30px] relative bottom-[20px]'>Sons of the forest</h3>
+            <button><img class='h-[30px] relative left-[250px] bottom-[20px]' src={carrito} alt="" /></button>
+            <h5 class='pl-[20px] relative bottom-[40px]'>$20.000 COP</h5>
+            </div>
+            <div class='bg-white rounded-lg h-[300px] mx-[10px]'>
+             <button><img class='w-[20px] text-right relative left-[265px] bg-black z-30 top-[5px]'  src={heart} alt="" /></button>
+            <img class='w-[300px] relative bottom-[30px] rounded-t-lg' src={forest} alt="" />
+            <ul class='flex relative bottom-[52px] left-[210px]'>
+                <li><img class='w-[16px]'  src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+            </ul>
+            <h3 class= 'pl-[20px] text-[25px] mb-[30px] relative bottom-[20px]'>Sons of the forest</h3>
+            <button><img class='h-[30px] relative left-[250px] bottom-[20px]' src={carrito} alt="" /></button>
+            <h5 class='pl-[20px] relative bottom-[40px]'>$20.000 COP</h5>
             </div>
 
-            <div class=' mx-[10px] p-[50px] border border-solid border-black rounded'>
-            <button><img class='w-[20px] text-right relative left-[195px] bottom-[20px]' src={info} alt="" /></button>
-            <h1 class ='text-center mb-[40px] border border-solid border-black rounded p-[80px]'>Imagen</h1>
-            <h3>Name</h3>
-            <h5>Price</h5>
-            <p>Description</p>
-            </div>
-
-            <div class='mx-[10px] p-[50px] border border-solid border-black rounded'>
-            <button><img class='w-[20px] text-right relative left-[195px] bottom-[20px]' src={info} alt="" /></button>
-            <h1 class ='text-center mb-[40px] border border-solid border-black rounded p-[80px]'>Imagen</h1>
-            <h3>Name</h3>
-            <h5>Price</h5>
-            <p>Description</p>
+            <div class='bg-white rounded-lg h-[300px] mx-[10px]'>
+             <button><img class='w-[20px] text-right relative left-[265px] bg-black z-30 top-[5px]'  src={heart} alt="" /></button>
+            <img class='w-[300px] relative bottom-[30px] rounded-t-lg' src={forest} alt="" />
+            <ul class='flex relative bottom-[52px] left-[210px]'>
+                <li><img class='w-[16px]'  src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starWhite} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+                <li><img class='w-[16px]' src={starBlue} alt="" /></li>
+            </ul>
+            <h3 class= 'pl-[20px] text-[25px] mb-[30px] relative bottom-[20px]'>Sons of the forest</h3>
+            <button><img class='h-[30px] relative left-[250px] bottom-[20px]' src={carrito} alt="" /></button>
+            <h5 class='pl-[20px] relative bottom-[40px]'>$20.000 COP</h5>
             </div>
 
         </div>
