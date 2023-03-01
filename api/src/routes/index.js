773a014bfdf3acf5b1ca5d4567f50dffe4887bee
Laow11 <Lautarogayoso17@gmail.com>
2023-02-28 21:28:14 -0300

    products update

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,8 @@
-const { Router } = require('express');
-const axios = require("axios");
-
+const { Router } = require("express");
+const productsRoutes = require("./products");
 
 const router = Router();
 
-
-
+router.use("/", productsRoutes);
 
 module.exports = router;
