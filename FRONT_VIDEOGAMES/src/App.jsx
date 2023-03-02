51cbe11b913558832d1f2c313d7bdf1a0ff78643
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-01 20:17:03 -0400

    cambio-estructura

--- FRONT_VIDEOGAMES/src/App.jsx
+++ FRONT_VIDEOGAMES/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './views/Home/Home'
+import CreateUser from './views/createUser/createUser'
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Prueba!</h1> 
-    </div>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/CreateGame" element={<CreateUser/>}/>
+        </Routes>  
+      </div>
   )
 }
