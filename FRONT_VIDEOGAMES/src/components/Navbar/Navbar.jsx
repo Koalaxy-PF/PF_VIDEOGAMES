28808a81da3f911dcbe71e192dc650cad9abd8bf
facundo-maciel <maci.facundo@gmail.com>
2023-03-02 15:58:52 -0300

    comente los iconos que no funcionan por el momento, y achique la navBar , tambien quite el searchBar del NavBar

--- FRONT_VIDEOGAMES/src/components/Navbar/Navbar.jsx
+++ FRONT_VIDEOGAMES/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import SearchBar from "../SearchBar/SearchBar";
 
 //importamos los iconos png 
 import heart from "../../assets/icons/corazon.png"
@@ -11,7 +10,7 @@ import koala from "../../assets/icons/koalaLogo.png"
 export default function NavBar(){
 
     return(
-        <div class="flex item-center justify-between flex-wrap bg-white p-8">
+        <div class="flex item-center justify-between flex-wrap bg-white p-4">
 
             <div class="flex item-center flex-shrink-0 text-black mr-6">
 
@@ -19,18 +18,16 @@ export default function NavBar(){
                 <span class="font-semibold text-5xl tracking-tight"> Koalaxy </span>
 
             </div>
-
-            <SearchBar/>
          
             
-            <div w-full flex-grow lg:flex lg:items-center lg:w-auto text-center>
+            {/* <div w-full flex-grow lg:flex lg:items-center lg:w-auto text-center>
 
                 <a class="block mt-4 lg:inline-block lg:mt-0 mr-4"> <img  class= " z-10 inset-y-0 my-auto h-9 bg-gray-300 rounded-full p-1"src = {trolley}/> </a>
                 <a class="block mt-4 lg:inline-block lg:mt-0 mr-4"> <img  class= " z-10 inset-y-0 my-auto h-9 bg-gray-300 rounded-full p-1"src = {heart}/> </a>
                 <a class="block mt-4 lg:inline-block lg:mt-0 mr-4">  <img  class= " z-10 inset-y-0 my-auto h-9 bg-gray-300 rounded-full p-1"src = {user}/> </a>
                 <a class="block mt-4 lg:inline-block lg:mt-0 mr-4"><img  class= " z-10 inset-y-0 my-auto h-9 bg-gray-300 rounded-full p-1"src = {darkMode}/> </a>
 
-            </div>
+            </div> */}
 
 
         </div>
