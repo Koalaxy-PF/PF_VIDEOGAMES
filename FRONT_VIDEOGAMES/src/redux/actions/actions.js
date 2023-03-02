fe8e92da68dd7666abcaa791b7d573df9b169198
facundo-maciel <maci.facundo@gmail.com>
2023-03-02 19:20:51 -0300

    hice las actions y los reducer

---
+++ FRONT_VIDEOGAMES/src/redux/actions/actions.js
@@ -0,0 +1,107 @@
+import axios from "axios";
+
+
+//action que trae todos los juegos
+export function GetGames(){
+
+    return async function(dispatch){
+        let Json = await axios.get(`http://localhost:3000/products`)
+        dispatch({
+            type: "GET_GAMES",
+            payload: Json.data
+        })
+    }
+}
+
+//action que trae juegos por id (sirve para el detail)
+export function GetGameById(id){
+
+    return async function(dispatch){
+      var json = await axios.get(`http://localhost:3000/products/${id}`)
+      return dispatch ({
+        type : "GET_BY_ID",
+        payload: json.data
+      })
+    }
+  
+}
+
+//action que trae todos los generos
+export function GetGenres(){
+
+    return async function(dispatch){
+        let Json = await axios.get(`http://localhost:3000/genres`)
+        dispatch({
+            type: "GET_GENRES",
+            payload: Json.data
+        })
+    }
+
+}
+
+//action PostGame sirve para el crear un juego
+export function PostGame(payload){
+
+    return async function(dispatch){
+        var json = axios.post(`http://localhost:3000/products`,payload)
+       return json
+   } 
+}
+
+//action que filtra por genero...
+export function FilterGenres(payload){
+
+    return{
+        type: "FILTER_CONTINENTS",
+        payload
+    }
+
+}
+
+//action que filtra por company...
+export function FilterCompany(payload){
+
+    return{
+        type: "FILTER_FER_GENRES",
+        payload
+    }
+
+}
+
+//action que odena alfabeticamente de forma ascendente y descendiente
+export function TidyAlphabetically(payload){
+  
+    return {
+        type : "ORDER_BY_NAME",
+        payload
+    }
+  
+}
+
+//action que odena por fecha de lanzamiento de forma ascendente y descendiente
+export function TidyReleased(payload){
+  
+    return {
+        type : "ORDER_BY_RELEASED",
+        payload
+    }
+  
+}
+
+//action que odena por precio de forma ascendente y descendiente
+export function TidyPrice(payload){
+
+    return{
+        type: "TIDY_PRICE",
+        payload
+    }
+}
+
+//action que limpia el estado
+export function Clean(){
+
+    return{
+        type: "CLEAN",
+        payload: []
+    }
+}
