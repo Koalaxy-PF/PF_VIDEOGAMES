881f74dd554277d18f422c5c282e17effd401fcd
Laow11 <Lautarogayoso17@gmail.com>
2023-03-01 22:09:29 -0300

    push lautaro prueba

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-const app = require('./src/app.js')
-const { conn } = require('./src/db.js');
+const app = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
-conn.sync({ force: true }).then(() => {   // si tenes en true, renueva la base de datos
+conn.sync({ force: true }).then(() => {
+  // si tenes en true, renueva la base de datos
 
   app.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
