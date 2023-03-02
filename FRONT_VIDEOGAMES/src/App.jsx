e816fda17ed4b7f815c1907328676171c193dc97
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-02 14:12:13 -0500

    cambios card y creacion createGame

--- FRONT_VIDEOGAMES/src/App.jsx
+++ FRONT_VIDEOGAMES/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home/Home";
 import CreateUser from "./views/createUser/createUser";
+import CreateGame from "./views/CreateGame/CreateGame";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/CreateGame" element={<CreateUser />} />
+        <Route exact path="/CreateGame" element={<CreateGame />} />
       </Routes>
     </div>
   );
