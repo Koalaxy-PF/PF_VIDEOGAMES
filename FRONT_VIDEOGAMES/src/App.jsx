a4b6e672b921e1076e3c5beaed8a0a620620e9df
facundo-maciel <maci.facundo@gmail.com>
2023-03-03 01:21:18 -0300

    manejo de errores

--- FRONT_VIDEOGAMES/src/App.jsx
+++ FRONT_VIDEOGAMES/src/App.jsx
@@ -1,30 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import {useDispatch} from "react-redux"
+import { useEffect  , useState } from "react";
+import {useDispatch , useSelector} from "react-redux"
 import Home from "./views/Home/Home";
 import CreateUser from "./views/createUser/createUser";
 import CreateGame from "./views/CreateGame/CreateGame";
 
-import {GetGames} from "../src/redux/actions/actions"
-
-
 
 export default function App() {
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(GetGames())
-  }, [dispatch])
-
 
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/CreateGame" element={<CreateGame />} />
-      </Routes>
+      </Routes>  
     </div>
+
+    
   );
 }
 
