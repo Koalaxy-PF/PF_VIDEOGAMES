af534bab1c4b58ac7c02f27eb33fd709ab29bd43
Laow11 <Lautarogayoso17@gmail.com>
2023-03-06 13:45:34 -0300

    changes

--- FRONT_VIDEOGAMES/src/redux/reducer/index.js
+++ FRONT_VIDEOGAMES/src/redux/reducer/index.js
@@ -1,170 +1,163 @@
 import {
-    GET_GAMES,
-    POST_GAME,
-    GET_BY_ID,
-    GET_GENRES,
-    FILTER_GENRES,
-    FILTER_PER_COMPANY,
-    ORDER_BY_NAME ,
-    ORDER_BY_RELEASED,
-    TIDY_PRICE ,
-    CLEAN } from "../actions/actions"
+  GET_GAMES,
+  POST_GAME,
+  GET_BY_ID,
+  GET_GENRES,
+  FILTER_GENRES,
+  FILTER_PER_COMPANY,
+  ORDER_BY_NAME,
+  ORDER_BY_RELEASED,
+  TIDY_PRICE,
+  CLEAN,
+} from "../actions/actions";
 
 const initialState = {
-    Games:[],
-    GamesCopy:[],
-    Genres:[],
-    details:[]
-}
+  Games: [],
+  GamesCopy: [],
+  Genres: [],
+  details: [],
+};
 
-function rootReducer(state = initialState, action){
-
-    switch(action.type){
-        case GET_GAMES:
-        return{
-            ...state,
-            Games: action.payload,
-            GamesCopy: action.payload,
-        }
-
-        case GET_GENRES:
-        return{
-            ...state,
-            Genres: action.payload
-        }
-
-        case POST_GAME:
-        return {
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_GAMES:
+      return {
         ...state,
-        };
+        Games: action.payload,
+        GamesCopy: action.payload,
+      };
 
-        //reducers de ordenamiento
+    case GET_GENRES:
+      return {
+        ...state,
+        Genres: action.payload,
+      };
 
-        case ORDER_BY_NAME :
-           let order = action.payload === 'asc' ? 
-              state.Games.sort(function(a,b) {
+    case POST_GAME:
+      return {
+        ...state,
+      };
 
-                if(a.name.toLowerCase() > b.name.toLowerCase()) {
+    //reducers de ordenamiento
 
-                    return 1
-                }
-                if( b.name.toLowerCase() > a.name.toLowerCase()){
-                    return -1
-                 }
-                return 0
-              }) : 
-              state.Games.sort(function(a,b) {
+    case ORDER_BY_NAME:
+      let order =
+        action.payload === "asc"
+          ? state.Games.sort(function (a, b) {
+              if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                return 1;
+              }
+              if (b.name.toLowerCase() > a.name.toLowerCase()) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.Games.sort(function (a, b) {
+              if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                return -1;
+              }
+              if (b.name.toLowerCase() > a.name.toLowerCase()) {
+                return 1;
+              }
+              return 0;
+            });
+      return {
+        ...state,
+        Games: order,
+      };
 
-                  if(a.name.toLowerCase() > b.name.toLowerCase()) {
-                    return -1
-                  }
-                  if( b.name.toLowerCase() > a.name.toLowerCase()){
-                    return 1
-                  }
-                return 0
-              })
-        return{
-            ...state,
-            Games: order
-        }
+    case ORDER_BY_RELEASED:
+      let orderByReleased =
+        action.payload === "asc"
+          ? state.Games.sort(function (a, b) {
+              if (a.released.toLowerCase() > b.released.toLowerCase()) {
+                return 1;
+              }
+              if (b.released.toLowerCase() > a.released.toLowerCase()) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.Games.sort(function (a, b) {
+              if (a.released.toLowerCase() > b.released.toLowerCase()) {
+                return -1;
+              }
+              if (b.released.toLowerCase() > a.released.toLowerCase()) {
+                return 1;
+              }
+              return 0;
+            });
+      return {
+        ...state,
+        Games: orderByReleased,
+      };
 
-        case ORDER_BY_RELEASED :
-            let orderByReleased = action.payload === 'asc' ? 
-               state.Games.sort(function(a,b) {
- 
-                 if(a.released.toLowerCase() > b.released.toLowerCase()) {
- 
-                     return 1
-                 }
-                 if( b.released.toLowerCase() > a.released.toLowerCase()){
-                     return -1
-                  }
-                 return 0
-               }) : 
-               state.Games.sort(function(a,b) {
- 
-                   if(a.released.toLowerCase() > b.released.toLowerCase()) {
-                     return -1
-                   }
-                   if( b.released.toLowerCase() > a.released.toLowerCase()){
-                     return 1
-                   }
-                 return 0
-               })
-         return{
-             ...state,
-             Games: orderByReleased
-         }
-        
-        case TIDY_PRICE:
-             
-            let TidyPrice = action.payload === 'min' ?
-    
-            state.Games.sort(function(a,b) {
-                      
-                if(a.price > b.price) {
-                  
-                    return 1
-                }
-                if( b.price > a.price){
-                    return -1
-                }
-                return 0
-            }) : 
-            state.Games.sort(function(a,b) {
-                if(a.price > b.price) {
-                    return -1
-                }
-                if( b.price > a.price){
-                    return 1
-                }
-                return 0
-             })
-        return{
-            ...state,
-            Games: TidyPrice
-        }
+    case TIDY_PRICE:
+      let TidyPrice =
+        action.payload === "min"
+          ? state.Games.sort(function (a, b) {
+              if (a.price > b.price) {
+                return 1;
+              }
+              if (b.price > a.price) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.Games.sort(function (a, b) {
+              if (a.price > b.price) {
+                return -1;
+              }
+              if (b.price > a.price) {
+                return 1;
+              }
+              return 0;
+            });
+      return {
+        ...state,
+        Games: TidyPrice,
+      };
 
-        //reduces de filtrados
+    //reduces de filtrados
 
-        case FILTER_PER_COMPANY:
-            const AllCom = state.GamesCopy
-            const TypeCompanyFilter = action.payload === "all"? AllCom : AllCom?.filter((t)=>t.company.includes(action.payload))
-        return {
-            ...state,
-            Games: TypeCompanyFilter
-        }
+    case FILTER_PER_COMPANY:
+      const AllCom = state.GamesCopy;
+      const TypeCompanyFilter =
+        action.payload === "all"
+          ? AllCom
+          : AllCom?.filter((t) => t.company.includes(action.payload));
+      return {
+        ...state,
+        Games: TypeCompanyFilter,
+      };
 
-        case FILTER_GENRES:
-            const AllGen = state.GamesCopy
-            const TypeGamesFilter = action.payload === "all"? AllGen : AllGen?.filter((t)=>t.genre.includes(action.payload))
-        return {
-            ...state,
-            Games: TypeGamesFilter
-        }
-        
+    case FILTER_GENRES:
+      const AllGen = state.GamesCopy;
+      const TypeGamesFilter =
+        action.payload === "all"
+          ? AllGen
+          : AllGen?.filter((t) => t.genre.includes(action.payload));
+      return {
+        ...state,
+        Games: TypeGamesFilter,
+      };
 
-        case GET_BY_ID:
-        return{
-            ...state,
-            details: action.payload
-        }
+    case GET_BY_ID:
+      return {
+        ...state,
+        details: action.payload,
+      };
 
-        case CLEAN:
-            return{
-                ...state,
-                details: action.payload
-        }
+    case CLEAN:
+      return {
+        ...state,
+        details: action.payload,
+      };
 
-
-        default:{
-            return state
-        }
-
+    default: {
+      return state;
     }
-
-
-
+  }
 }
 
-export default rootReducer
+export default rootReducer;
