53e7322365c9636fe96bbfa6b8f378e9873dfd53
Akadus23 <108701227+Akadus23@users.noreply.github.com>
2023-03-09 13:19:21 -0500

    new pagination

--- FRONT_VIDEOGAMES/src/components/Pagination/Pagination.jsx
+++ FRONT_VIDEOGAMES/src/components/Pagination/Pagination.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 
-export default function Pagination({gamesPerPage, allGames, pagination}){
-    const pagesNumber = [];
-    for(let i = 1; i <= Math.ceil(allGames/gamesPerPage); i++){
-        pagesNumber.push(i)
-    };
+export default function Pagination({props}){
+    const { currentPage, maxPageLimit, minPageLimit } = props;
+    const totalPages = props.response.totalPages-1;
+    const data = props.response.data;
+    const pages = [];
+    for (let i = 1; i < totalPages; i++) {
+        pages.push(i);
+    }
+
+    const pageNumbers = pages.map(page => {
+        if(page <= maxPageLimit && page > minPageLimit) {
+            return(
+                <li key={page} id={page} onClick={handlePageClick} className={currentPage === page ? 'active' : null} >
+                    {page}
+                </li>
+            );
+        } else {
+            return null;
+        }
+    })
+
+    let pageIncrementEllipses = null;
+    if(pages.length > maxPageLimit){
+        pageIncrementEllipses = <li onClick={handleNextClick}>&hellip;</li>
+    }
+    let pageDecrementEllipses = null;
+    if(minPageLimit >= 1){
+        pageDecrementEllipses = <li onClick={handlePrevClick} >&hellip;</li>
+    }
+    // const renderData = (data) =>{
+    //     return(
+    //         <ul>
+    //             {data.map((d) => 
+    //             <li key={d['_id']} ></li>)}
+    //         </ul>
+    //     )
+    // }
 
     return(
         <nav className="flex flex-row text-center">
