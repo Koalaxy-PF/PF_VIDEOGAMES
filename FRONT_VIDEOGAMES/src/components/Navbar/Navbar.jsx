ece71b841cc589e3f7f5e74f4bd86dbb5135b58c
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-11 09:44:46 -0400

    validaciones

--- FRONT_VIDEOGAMES/src/components/Navbar/Navbar.jsx
+++ FRONT_VIDEOGAMES/src/components/Navbar/Navbar.jsx
@@ -9,8 +9,8 @@ import koala from "../../assets/icons/koalaLogo.png"
 export default function NavBar(){
 
     return(
-        <div>
-                    <div class="flex item-center justify-between flex-wrap bg-white p-4">
+        <div className="">
+                    <div class="flex item-center justify-between flex-wrap p-4">
 
                     <div class="flex item-center flex-shrink-0 text-black mr-6 sticky ">
 
--- FRONT_VIDEOGAMES/src/views/createUser/validators.jsx
+++ FRONT_VIDEOGAMES/src/views/createUser/validators.jsx
@@ -1,16 +1,20 @@
 
 const edadValidator = (value) => {
+
+    console.log(value);
+
     const fechaActual = new Date();
     const anoActual = parseInt(fechaActual.getFullYear());
     const mesActual = parseInt(fechaActual.getMonth()) + 1;
     const diaActual = parseInt(fechaActual.getDate());
-
    
     const diaNacimiento = parseInt(String(value).substring(0, 2));
     const mesNacimiento = parseInt(String(value).substring(3, 5));
     const anoNacimiento = parseInt(String(value).substring(6, 10));
 
     let edad = anoActual - anoNacimiento;
+
+
     if (mesActual < mesNacimiento) {
         edad--;
     } else if (mesActual === mesNacimiento) {
