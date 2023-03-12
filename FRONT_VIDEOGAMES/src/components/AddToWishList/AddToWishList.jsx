649896f09eb32f797843c37f820c0d899d82e11f
Akadus23 <108701227+Akadus23@users.noreply.github.com>
2023-03-12 17:36:28 -0500

    wishlist actions fixed

--- FRONT_VIDEOGAMES/src/components/AddToWishList/AddToWishList.jsx
+++ FRONT_VIDEOGAMES/src/components/AddToWishList/AddToWishList.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { PostWishList } from "../../redux/actions/actions";
 
 export default function AddToWishList({productId}){
+    const User = useSelector((state) => state.user)
+
     const [added] = useState(false)
 
 
-const handleClick = () => {
-    dispatch(PostWishList())
+const handleClick = (e) => {
+    dispatch(PostWishList(e))
 }
 
     return(
