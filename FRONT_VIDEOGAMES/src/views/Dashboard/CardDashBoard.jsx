279e7a3dc1e8c02787fa7ea0ed524a97d8bfed0b
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-16 21:53:28 -0500

    conexion back DashBoard

--- FRONT_VIDEOGAMES/src/views/Dashboard/CardDashBoard.jsx
+++ FRONT_VIDEOGAMES/src/views/Dashboard/CardDashBoard.jsx
@@ -1,38 +1,19 @@
 import React from "react";
-import AddToWishList from "../../components/AddToWishList/AddToWishList";
+
+
 
 
 
  export default function CardDashBoard({index, id, name, img, calification, price, genre}) {
   return (
-    <div class="flex h-[350px] mx-[20px]">
-      <div>
-      <div>
-       <img src={img} alt=" " width={"300px"} height={"200px"} />
-       <div>
-       <h1 class="text-[21px] text-left ml-[10px] mt-[10px]">{name}</h1>
-       </div>
-
-       <div class='flex flex-wrap ml-[10px] mt-[5px]'>
-       <p class='w-full text-left'><strong>{calification}/5 <span class='text-[#00E1D8] relative top-[1px] left-[2px]'><ion-icon name="star"></ion-icon></span> </strong></p>
-           {/*  <p class='w-full text-left'>Stock: <strong>{stock}</strong></p> */}
-        <div>
-          <AddToWishList 
-          productId={id}
-          />
-        </div>
-             <p class='w-full text-left'>{genre}</p>  
-
-
-       </div>
-
-       <div class='text-right'> 
-       <h5 class='text-right text-whit'><span class='bg-gray-300 px-[10px] py-[4px] rounded-l reounde-t'>$ {price}</span></h5>
-       </div>
-
-         </div>
-       </div>
-     </div>
+    <div class='flex text-left bg-white items-center justify-around py-[10px] pt-[10px] w-[500px]'>
+    <img class='bg-white rounded-full w-[70px] h-[70px] text-left mr-[10px] ml-[30px] object-cover' src={img}></img>
+    <div class='w-[200px] bg-white'>
+    <h5 class='mr-[200px] text-[15px] w-[200px]'>{name}</h5>
+    <h5 class='text-[12px]'>${price}</h5>
+    </div>
+    <span class='mr-[30px]'><ion-icon name="create"></ion-icon></span>
+  </div>
     
   );
  }
