257cee50e140972cd74b02acce9d650d04c3dbf8
AgusCorcico <corcicoagustin@gmail.com>
2023-03-16 23:14:28 -0300

    reviews finalizado

--- api/src/routes/reviewRoutes/post.js
+++ api/src/routes/reviewRoutes/post.js
@@ -1,24 +1,41 @@
 const { Router } = require("express");
 const router = Router();
 const { Review } = require("../../db");
+const  {  Productlibrary, Library, User }  = require("../../db");
 
 
 router.post("/", async (req, res) => {
-    try {
-        const { userId, productId, calification, comment } = req.body;
+    const { userId, productId, calification, comment } = req.body;
+
+    const user = await User.findOne({
+        where: { id: userId}
+    });
+
+    const library = await Library.findOne({
+        where: { id: user.libraryId },
+        include: { model: Productlibrary}
+    });
+
+    const findProduct = await library.productlibraries?.find(e => e.productId == productId);
 
 
-        let newComment = await Review.create({
-            userId,
-            productId,
-            calification,
-            comment
-        })
-        res.status(200).send(newComment)
-/*         res.status(200).send('New comment has been post successfully') */
-    } catch (error) {
-        console.log(error)
+
+    if(findProduct){
+        try {
+            let newComment = await Review.create({
+                userId,
+                productId,
+                calification,
+                comment
+            })
+            res.status(200).send(newComment)
+        } catch (error) {
+            console.log(error)
+        }
+    }else{
+        res.status(400).send('Comment only those who bought the game! Sorry.')
     }
+
 })
 
 
