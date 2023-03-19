ce8250e1ec8cb6168adf4b55a6a577baba12ce82
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-19 15:57:43 -0400

    fix: logout

--- FRONT_VIDEOGAMES/src/views/QA/QA.jsx
+++ FRONT_VIDEOGAMES/src/views/QA/QA.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/SideBar/Sidebar'
 import Footer from '../../components/Footer/Footer'
 
-export default function QA(){
+export default function QA() {
   return(
 
     <div className=''>
@@ -12,9 +12,11 @@ export default function QA(){
             <Navbar />
         </div>
 
-        <div className='flex w-full h-[calc(100vh-5.1rem)]'>
+        <div className='flex w-full h-[calc(100vh-5.1rem)] bg-gray-100'>
 
-            <section className="bg-gray-200 text-gray-500 w-full h-full">
+            <Sidebar />
+
+            <section className="bg-slate-200 text-gray-500 w-full h-full">
 	            <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
 		            <h2 className="mb-5 text-4xl  py-2 font-bold leading-none text-center sm:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">Frequently Asked Questions</h2>
 		            <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
@@ -26,6 +28,7 @@ export default function QA(){
 				</div>
 
 			</details>
+
 			<details>
 				<summary className="py-2 outline-none cursor-pointer focus:underline">Modi dolorem veritatis culpa quos consequuntur beatae itaque excepturi perspiciatis?</summary>
 				<div className="px-4 pb-4">
