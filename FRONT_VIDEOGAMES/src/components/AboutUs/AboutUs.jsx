8972210a6aca756c7babe2617873f712fbdedcf1
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-20 19:35:05 -0400

    fix: pay

--- FRONT_VIDEOGAMES/src/components/AboutUs/AboutUs.jsx
+++ FRONT_VIDEOGAMES/src/components/AboutUs/AboutUs.jsx
@@ -9,18 +9,18 @@ export default function AboutUs(){
 
   
   return (
-    <div className="AboutUs">
+    <div class="bg-cover bg-no-repeat" style={{backgroundImage: `url('https://www.xtrafondos.com/descargar.php?id=4047&resolucion=3840x2400')`, backgroundSize: 'cover'}}>
 
         <div>
             <Navbar />
         </div>
 
 
-		<div className='flex w-full h-full bg-gray-100'>
+		<div className='flex w-full h-full'>
 
             <Sidebar />
 
-            <section className="py-1 bg-gray-100 text-gray-100 w-full">
+            <section className="py-1 text-gray-100 w-full">
 	            <div className="container p-4 mx-auto space-y-16 sm:p-10">
 		<div className="space-y-4">
 			<h1 className="text-4xl font-bold leading-none text-center sm:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
@@ -38,8 +38,8 @@ export default function AboutUs(){
 						<div className="space-y-4">
 							<img alt="img1" className="object-cover h-56 w-52 shadow-xl shadow-slate-500 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src={element.image} />
 							<div className="flex flex-col items-center">
-							<h4 className="text-xl font-semibold text-gray-600">{element.name}</h4>
-							<p className="text-sm text-gray-400">Full Stack Developer</p>
+							<h4 className="text-xl font-semibold text-slate-100">{element.name}</h4>
+							<p className="text-lg text-gray-300">Full Stack Developer</p>
 							<div className="flex mt-2 space-x-2">
 								<a rel="noopener noreferrer" href={element.Linkedin} title="LinkedIn" className="text-gray-400">
 									<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-4 h-4">
