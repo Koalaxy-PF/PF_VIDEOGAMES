8a69534853df057f7162ade714bc319e6aeb1144
facundo-maciel <maci.facundo@gmail.com>
2023-03-19 21:28:24 -0300

    cambios en el sideBar del dashBoard

--- FRONT_VIDEOGAMES/src/views/Dashboard/sideBarDashboard.jsx
+++ FRONT_VIDEOGAMES/src/views/Dashboard/sideBarDashboard.jsx
@@ -1,24 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux"
 import users from "../../assets/icons/users.png"
 import games from "../../assets/icons/games.png"
 import home from "../../assets/icons/home.png"
-import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom"
+import user from "../../assets/icons/usuario.png"
 
 export default function SideBarDashBoard(){
 
-    // const User = useSelector((state) => state.user);
+    const User = useSelector((state) => state.user);
 
     return(
         <div className="bg-gray-900 h-full  fixed  lg:static w-[100%] top-0 left-0 ">
 
-            <div className="justify-center item-center  flex flex-col p-8 gap-2 h-[30vh] place-content-center px-20">
-                <img src="https://img.freepik.com/foto-gratis/joven-feliz-pie-aislado_171337-1127.jpg?w=2000" className="w-20 text-center  h-20 objet-cover  rounded-full ring-2 ring-gray-300"/>
-                <h1 className="text-white text-2xl font-bolt text-center">Facundo</h1>
-                <p className="text-white text-xl font-bolt bg-gray-200 py-1 px-3 rounded-full text-center">Admin</p>
-            </div>
 
-            {/* {User?.map((e) =>{
+            {
+                 Object.keys(User).length === 0 ?
+
+                 <div className="justify-center item-center  flex flex-col p-8 gap-2 h-[30vh] place-content-center px-20">
+                 <img src={user} className="w-20 text-center  h-20 objet-cover  rounded-full ring-2 ring-gray-300"/>
+                 <h1 className="text-white text-2xl font-bolt text-center">Usuario</h1>
+                 <p className="text-white text-xl font-bolt bg-gray-200 py-1 px-3 rounded-full text-center">Admin</p>
+                 </div>
+
+                 :
+
+                 <div className="justify-center item-center  flex flex-col p-8 gap-2 h-[30vh] place-content-center px-20">
+                 <img src={User.user.img} className="w-20 text-center  h-20 objet-cover  rounded-full ring-2 ring-gray-300"/>
+                 <h1 className="text-white text-2xl font-bolt text-center">{User.user.name}</h1>
+                 <p className="text-white text-xl font-bolt bg-gray-200 py-1 px-3 rounded-full text-center">Admin</p>
+                </div>
+
+            }
+
+
+            {/* {User.user?.map((e) =>{
                 return(
 
             <div className="flex flex-col justify-center item-center p-8 gap-2 h-[30vh] ">
