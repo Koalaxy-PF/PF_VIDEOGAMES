02d7eb153492567bf9b71366572a20a2825f92dc
facundo-maciel <maci.facundo@gmail.com>
2023-03-20 22:07:51 -0300

    Solucioné el User ya me renderiza

--- FRONT_VIDEOGAMES/src/views/Dashboard/Users/DashBoardUser.jsx
+++ FRONT_VIDEOGAMES/src/views/Dashboard/Users/DashBoardUser.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React ,{useEffect}from "react";
 import { useSelector, useDispatch } from "react-redux";
 import SideBarDashBoard from "../sideBarDashboard";
-import CardUserDashBoard from "./CardUserDashBoard";
 import {GetUsers} from "../../../redux/actions/actions";
 import logo from '../../../assets/icons/koalaLogo.png'
+import CardUserDashBoard from "./CardsUserDashBoard";
 
 
 export default function DashBoardUser(){
 
-    const allUsers = useSelector((state) => state.users);
+
+    // const dispatch = useDispatch();
+    // const allUsers = useSelector((state) => state.users);
+
+    // useEffect(() => {
+    //     dispatch(GetUsers());
+    //   }, [dispatch]); 
 
   return(
-    <div class='grid grid-cols-6 min-h-screen '>
+    <div class='grid grid-cols-6 min-h-screen overflow-y-hidden '  >
 
     <div class='col-span-1 bg-slate-400 text-center w-full'>
         <SideBarDashBoard/>
     </div>
 
-    <div class='col-span-5  items-center relative' style={{backgroundImage: `url('https://www.xtrafondos.com/descargar.php?id=4047&resolucion=3840x2400')`, backgroundSize: 'cover'}}>
+    <div class='col-span-5 items-center relative' style={{backgroundImage: `url('https://www.xtrafondos.com/descargar.php?id=4047&resolucion=3840x2400')`, backgroundSize: 'cover'}} >
 
         <div class='mx-[30px] mt-[20px] bg-white mb-[20px] p-[10px] rounded flex items-center'>
             <img class='w-[50px]' src={logo} alt="" />
             <h1 class='font-bold ml-[10px] text-[25px]'>DashBoard Users Admin</h1>
-            <span class='relative left-[680px] text-[20px]'><ion-icon name="notifications"></ion-icon></span>
+          
         </div>
 
         <div class='flex justify-around relative bottom-[10px]'>
@@ -30,14 +36,14 @@ export default function DashBoardUser(){
             <div class='justify-center text-center bg-white py-[20px] rounded'>
             <div class='flex justify-around'>
             <h1 class='relative right-[48px] font-bold text-[20px]'>Users</h1>
-            <span class='relative left-[60px]'>{allUsers.length}</span>
             </div>
-            <div class='flex text-left bg-white items-center justify-around py-[10px] mt-[10px] flex-wrap w-[900px]'>
-            <CardUserDashBoard/>
+
+            <div class='flex text-left bg-white items-center justify-around py-[10px] mt-[10px] flex-wrap w-[1000px]'>
+                
+                <CardUserDashBoard/>
+
             </div>
-            <div>
-            <button class='py-[10px] px-[10px] mt-[20px] rounded border-blue-900 border-[1px]'>See all the games</button>
-            </div>
+
             </div>
 
         </div>
