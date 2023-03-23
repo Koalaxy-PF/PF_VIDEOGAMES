942273e2d5388358fd8da92be848f07b8fdf9d13
facundo-maciel <maci.facundo@gmail.com>
2023-03-23 12:56:04 -0300

    nueva validacion

--- FRONT_VIDEOGAMES/src/components/FormLogin/FormLogin.jsx
+++ FRONT_VIDEOGAMES/src/components/FormLogin/FormLogin.jsx
@@ -73,7 +73,6 @@ export default function LoginForm(){
               })
     }
         
-
     return(
         <div class='min-height-full flex  justify-center'style={{backgroundImage: `url('https://www.xtrafondos.com/descargar.php?id=4047&resolucion=3840x2400')`, backgroundSize: 'cover'}} >
             <div class='hidden lg:block relative h-full'>
