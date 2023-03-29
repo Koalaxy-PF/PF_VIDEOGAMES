2bffb0fdb69853f69ef101309a364230957957c3
Agustina <aguavalos@hotmail.com>
2023-03-29 20:43:08 -0300

    algo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -48,8 +48,7 @@ const getReview = require('./reviewRoutes/get');
 const updateReview = require('./reviewRoutes/put');
 const deleteReview = require('./reviewRoutes/delete');
 
-const postContactus = require("./contactus/post.js")
-const getContactus = require('./contactus/get.js')
+
 
 
 const getallOrders = require('./orderRoutes/getAll.js')
